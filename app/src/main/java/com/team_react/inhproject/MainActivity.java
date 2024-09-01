039666a0c233aff1325fe23ddf006452b7180151
darkengine <darkwildahcker@gmail.com>
2024-09-01 21:55:56 +0530

    Foundation UI

--- app/src/main/java/com/team_react/inhproject/MainActivity.java
+++ app/src/main/java/com/team_react/inhproject/MainActivity.java
@@ -1,19 +1,58 @@
 package com.team_react.inhproject;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.material.button.MaterialButton;
+import com.team_react.inhproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        MaterialButton explore = binding.explore;
+
+        explore.setIcon(ContextCompat.getDrawable(this, R.drawable.explore));
+        explore.setIconGravity(MaterialButton.ICON_GRAVITY_END);
+        explore.setIconTint(ColorStateList.valueOf(Color.parseColor("#333A73")));
+
+        explore.setOnClickListener(view -> {
+            bgAnim(explore, 0x80FBA834, 0xFF333A73, 1000);
+            iconAnim(explore, 0xFF333A73, 0xFFFBA834, 1000);
+        });
+    }
+
+
+    public void bgAnim(MaterialButton button, int startColor, int endColor, int duration) {
+        ValueAnimator colorAnimator = ValueAnimator.ofObject(new ArgbEvaluator(), startColor, endColor);
+        colorAnimator.setDuration(duration);
+        colorAnimator.addUpdateListener(animator -> {
+            button.setBackgroundColor((int) animator.getAnimatedValue());
+        });
+        colorAnimator.start();
+    }
+
+    public void iconAnim(MaterialButton button, int startColor, int endColor, int duration) {
+        ValueAnimator colorAnimator = ValueAnimator.ofObject(new ArgbEvaluator(), startColor, endColor);
+        colorAnimator.setDuration(duration);
+        colorAnimator.addUpdateListener(animator -> {
+            button.setIconTint(ColorStateList.valueOf((int) animator.getAnimatedValue()));
+            button.setIconTintMode(PorterDuff.Mode.SRC_IN);
+            button.setTextColor((int) animator.getAnimatedValue());
+        });
+        colorAnimator.start();
     }
 }
